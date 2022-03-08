2ea9d8f5db2b6bf369a1374977505c65686065bf
João Araújo <joaoaraujo.78@gmail.com>
2022-03-08 09:39:18 +0000

    working version

--- script.js
+++ script.js
@@ -1,48 +1,70 @@
-const container = document.getElementById('container');
-let hSize = 16;
-let vSize = 16;
-let mode = 'black';
-let rgb;
+let htmlStyles = window.getComputedStyle(document.querySelector('html'));
+let rowNum = parseInt(htmlStyles.getPropertyValue('--rowNum'));
 
-for (let i=0; i<hSize*vSize; i++) {
-    const grid = document.createElement('div');
-    grid.setAttribute('class', 'grid');
-    container.appendChild(grid);
+const container = document.getElementById('container');
+let mode = 'black';
+
+generateCanvas(16);
+
+function generateCanvas(size) {
+    sizePx = size + 'px';
+    console.log(sizePx);
+    document.documentElement.style.setProperty('--gridSize', sizePx);
+    document.documentElement.style.setProperty('--size', size);
+    for (let i=0; i<size; i++) {
+        for (let j=0; j<size; j++) {
+        const grid = document.createElement('div');
+        grid.setAttribute('class', 'grid');
+        container.appendChild(grid);
+        }
+    }
+
+    color()
 }
 
-const fullGrid = document.querySelectorAll('.grid'); 
 
+
+
+function color () {
+
+const fullGrid = document.querySelectorAll('.grid'); 
 fullGrid.forEach(function (e) {
-    e.addEventListener('mousemove', function (et) {
+    e.addEventListener('mouseover', function (et) {
         if (mode == 'black') {
         et.target.style.backgroundColor = 'black';
-        console.log('hello')
         } else if (mode == 'rgb') {
-        rgbGenerator()
-        et.target.style.backgroundColor = 'blue';
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            let rgb = "rgb(" + r + ", " + g + ", " + b +")";
+            et.target.style.backgroundColor = rgb;
         }
     });
 });
+}
 
 const clearBtn = document.getElementById('clear');
 
 clearBtn.addEventListener('click', () => {
-    fullGrid.forEach(function (e) {
-        e.style.backgroundColor = 'white';  
+    const fullGrid = document.querySelectorAll('.grid'); 
+    size = prompt('Select number of squares: (less than 100)');
+    fullGrid.forEach(box => {
+        box.remove();
     });
+    
+    generateCanvas(size);
 });
 
 const rgbBtn = document.getElementById('RGB');
 
 rgbBtn.addEventListener('click', () => {
     mode = 'rgb';
-    console.log('bassey');
 })
 
 function rgbGenerator () {
-    let r = Math.floor(Math.random() * 255)
-    let g = Math.floor(Math.random() * 255)
-    let b = Math.floor(Math.random() * 255)
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
     let rgb = "rgb(" + r + ", " + g + ", " + b +")";
     return rgb;
 }
